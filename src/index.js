2457527aa57db43ce66bfc1572da8db5e5371a44
Tobias <tobias.c.ziegler@gmail.com>
2023-02-19 16:54:22 +0100

    Update prod uri

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GlobalStyles from "./styles";
-import Pages from "./pages";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GlobalStyles from './styles';
+import Pages from './pages';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: "https://space-courses-api.herokuapp.com/", // change to YOUR own production server
+  uri: 'https://odyssey-lift-off-server-production-6d6a.up.railway.app/',
   cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
 });
 
 ReactDOM.render(
@@ -16,5 +14,5 @@ ReactDOM.render(
     <GlobalStyles />
     <Pages />
   </ApolloProvider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
